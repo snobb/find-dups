e971ce6601f800c6db4ccdc019703a3557a6cb3e
Alex Kozadaev <akozadaev at yahoo com>
2014-11-23 14:07:06 +0000

    py-1.03: fixing the reporing bugs where some files were reorted several times

--- pp/finddups.py
+++ pp/finddups.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
 #
-# { finddups.py }
-# Copyright (C) 2013 Alex Kozadaev [akozadaev at yahoo com]
+# finddups.py
+# Author: Alex Kozadaev (2013)
 #
 
 import sys, os, hashlib
 
-VERSION = "1.02"
+VERSION = "1.03"
 
-record={}
+record = {}
+
 def get_md5sum(filename):
     md5 = hashlib.md5()
     with open(filename,"rb") as f:
@@ -16,18 +17,22 @@ def get_md5sum(filename):
             md5.update(chunk)
     return md5.hexdigest()
 
+
 def find_dups(top):
     global record
     for root, dirs, files in os.walk(top):
         for name in files:
             path = os.path.join(root, name)
-            md5sum = get_md5sum(path)
-            record.setdefault(md5sum, []).append("\"{}\"".format(path))
+            if not os.path.islink(path):
+                md5sum = get_md5sum(path)
+                record.setdefault(md5sum, []).append("{}".format(path))
     return record
+
 
 def usage(prog):
     print "{} v{} [python edition]".format(prog, VERSION)
     print "Usage: finddup [directory/files to search]\n"
+
 
 if __name__ == "__main__":
     prog = sys.argv.pop(0)
@@ -38,12 +43,17 @@ if __name__ == "__main__":
     try:
         for directory in sys.argv:
             db = find_dups(directory)
-            for chksum in db.iterkeys():
-                if len(db[chksum]) > 1:
-                    print "{}\n\t".format(chksum),
-                    print "\n\t".join(db[chksum])
+
+        for chksum in record.iterkeys():
+            if len(db[chksum]) > 1:
+                print "{}\n\t".format(chksum),
+                print "\n\t".join(db[chksum])
+    except IOError as e:
+        print "ERROR: {}".format(e.message)
+        exit(1)
     except KeyboardInterrupt:
         print "interrupted..."
         exit(1)
 
-# vim: set ts=4 sts=4 sw=4 tw=80 ai smarttab et list
+
+# vim: ts=4 sts=4 sw=4 tw=80 ai smarttab et fo=rtcq list
