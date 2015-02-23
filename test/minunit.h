22dfb2ed5ffff6d67d991a4f3785f410aa613d76
Alex Kozadaev <akozadaev at yahoo com>
2015-02-23 22:31:11 +0000

    c: updating minunit.h framework

--- test/minunit.h
+++ test/minunit.h
@@ -6,29 +6,43 @@
 
 #include "minunit.h"
 
-char *test_dlopen()
+static void setup(void)
+{
+    // setup code here
+}
+
+static void teardown(void)
+{
+    // teardown code here
+}
+
+static char *test_dlopen()
 {
     return NULL;
 }
 
-char *test_functions()
+static char *test_functions(void)
 {
     return NULL;
 }
 
-char *test_failures()
+static char *test_failures(void)
 {
     return NULL;
 }
 
-char *test_dlclose()
+static char *test_dlclose(void)
 {
     return NULL;
 }
 
-char *all_tests() {
-    mu_suite_start();
+static char *all_tests(void)
+{
+    // registing setup and teardown functions
+    mu_register_setup(setup)
+    mu_register_teardown(teardown);
 
+    // running tests
     mu_run_test(test_dlopen);
     mu_run_test(test_functions);
     mu_run_test(test_failures);
@@ -37,7 +51,7 @@ char *all_tests() {
     return NULL;
 }
 
-RUN_TESTS(all_tests);
+RUN_TESTS(all_tests)
 
 ========================================================================= */
 
@@ -46,33 +60,50 @@ RUN_TESTS(all_tests);
 #include <stdlib.h>
 
 #ifdef DEBUG
-#       define debug(fmt, ...) fprintf(stderr,"[DEBUG] %s:%d " fmt,\
+#       define debug(fmt, ...) fprintf(stderr,"[DEBUG] %s:%d " fmt,     \
                                        __FILE__, __LINE__, __VA_ARGS__)
 #else
 #       define debug(fmt, ...)
 #endif
 
-#define mu_suite_start() char *message __attribute__ ((unused)) = NULL;
-#define mu_assert(test, message) if (!(test)) {\
-                                    if (message)\
-                                       return message " (" #test ")";\
-                                    else return message;\
-                                 }
-#define mu_run_test(test) { char *message = test(); tests_run++; \
-                                     if (message) return message; }
+#define mu_assert(test)                                                 \
+    if (!(test)) { return #test; }
 
-#define RUN_TESTS(name) int main(int argc, char *argv[]) {\
-    fprintf(stderr, "----\nRUNNING: %s\n", argv[0]);\
-    char *result = name();\
-    if (result != 0) {\
-        fprintf(stderr, "FAILED: %s\n", result);\
-    } else {\
-        fprintf(stderr, "ALL TESTS PASSED\n");\
-    }\
-    fprintf(stderr, "Tests run: %d\n", tests_run);\
-    exit(result != 0);\
-}
+#define mu_assert_msg(test, msg)                                        \
+    if (!(test)) {                                                      \
+        if (msg) { return msg " (" #test ")"; }                         \
+        else { return msg; }                                            \
+    }
+
+#define mu_run_test(test)                                               \
+    {                                                                   \
+        fprintf(stderr, "-- running: %s\n", #test);                     \
+        if (setup_func) { setup_func(); }                               \
+        char *msg = test();                                             \
+        tests_run++;                                                    \
+        if (teardown_func) { teardown_func(); }                         \
+        if (msg) return msg;                                            \
+    }
+
+#define mu_register_setup(setup)           { setup_func = setup; }
+#define mu_register_teardown(teardown)     { teardown_func = teardown; }
+
+#define RUN_TESTS(name)                                                 \
+    int main(int argc, char *argv[]) {                                  \
+        fprintf(stderr, "----\nRUNNING: %s\n", argv[0]);                \
+        char *result = name();                                          \
+        if (result != 0) {                                              \
+            fprintf(stderr, "FAILED: %s\n", result);                    \
+        } else {                                                        \
+            fprintf(stderr, "ALL TESTS PASSED\n");                      \
+        }                                                               \
+        fprintf(stderr, "Tests run: %d\n", tests_run);                  \
+        exit(result != 0);                                              \
+    }
 
 int tests_run;
+
+void (*setup_func)(void) = NULL;
+void (*teardown_func)(void) = NULL;
 
 #endif /* MINUNIT_H */
--- test/test_md5.c
+++ test/test_md5.c
@@ -8,7 +8,7 @@
 
 #define DEBUG 1
 
-char *test_md5_compare()
+static char *test_md5_compare()
 {
     int res = 0;
     unsigned char m1[] = {
@@ -29,14 +29,14 @@ char *test_md5_compare()
     };
 
     res = md5_compare(m1, m2);
-    mu_assert(res < 0, "Expected m1 is less the m2")
+    mu_assert_msg(res < 0, "Expected m1 is less the m2")
     res = md5_compare(m3, m4);
-    mu_assert(res < 0, "Expected m3 is less the m4")
+    mu_assert_msg(res < 0, "Expected m3 is less the m4")
 
     return NULL;
 }
 
-char *test_md5_get()
+static char *test_md5_get()
 {
     unsigned char got[16], expected[16] = {
         0x46, 0x3b, 0x70, 0x08, 0xd0, 0xbd, 0xe3, 0xa7,
@@ -46,15 +46,26 @@ char *test_md5_get()
     char md5got[33];
 
     md5_get("test/testfile", got);
-    mu_assert(md5_compare(got, expected) == 0, "md5sum mismatch")
+    mu_assert_msg(md5_compare(got, expected) == 0, "md5sum mismatch")
 
     for (int i = 0; i < 16; i++) {
         sprintf(md5got+(i*2), "%02x", got[i]);
     }
-    mu_assert(strncmp(md5exp, md5got, 32) == 0, "md5sum mismatch")
+    mu_assert_msg(strncmp(md5exp, md5got, 32) == 0, "md5sum mismatch")
 
     return NULL;
 }
+
+static char *all_tests() {
+    /* test_md5.c */
+    mu_run_test(test_md5_compare);
+    mu_run_test(test_md5_get);
+
+    return NULL;
+}
+
+RUN_TESTS(all_tests)
+
 
 /* vim: ts=4 sts=8 sw=4 smarttab et si tw=80 ci cino+=t0(0 list */
 
